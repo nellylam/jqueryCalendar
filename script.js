cdadf55a10bcf7befbeda264a8b813609da0c06c
Nelly Lam <nellyannlam@gmail.com>
2014-08-30 15:14:22 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+
+	var days = [
+		{
+			name : 'Ind Day',
+			day : 16
+		},
+		{
+			name : 'Labor Day',
+			day : 1
+		}
+	];
+
+    var t = $('table');
+    t.addClass('classy');
+
+    // create function to insert things into the calendar
+    function insertEvent(name, day){
+		if (name.length) {
+			console.log('name is ' + name);
+		} else {
+			console.error('no name given');
+			return; //this exits the function if no name given
+		}
+
+		console.log(day);
+
+		if (day > 0) {
+			console.log('day is ' + day);
+		} else {
+			console.error('no day given');
+			return;
+		}
+
+		var offset = 1;
+
+		$($('td')[offset + (day - 1)]).append('<span class="event">' + name + '</span>');
+	}; // end function insertEvent
+
+    // on click function
+
+	$('#submitter').on('click', function(evt) {
+		evt.preventDefault();
+
+		console.log('clicked button!');
+
+		var name = $('[name="eventName"]').val();
+
+		var day = $('[name="eventDay"]').val();
+
+		insertEvent(name, day);
+    });
+
+    $('td').on('click', function(event){
+		console.log(event);
+
+		var day = $(event.target).children('.date').text();
+
+		$('[name="eventDay"]').val(day); //val is a getter/setter
+    });
+
+    //initial setup
+    $.each(days, function(index, date){
+    	insertEvent(date.name, date.day);
+    });
+
+	// $('#eventName').on('keyup', function(event){
+	// 	console.log($('#eventName').val());
+ //    });
+	
+});
